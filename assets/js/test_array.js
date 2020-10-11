64c8f633dd2d6965282e07c8753dd3b5d5cd220a
Mystic Kitty <djabranton@gmail.com>
2020-10-10 17:51:40 -0700

    Asynch incrmentation through quiz questions, score, results, local storage implemented.

--- assets/js/test_array.js
+++ assets/js/test_array.js
@@ -1,4 +1,6 @@
-document.getElementById("start").addEventListener("click", takeQuiz);
+var highScores = JSON.parse(localStorage.getItem("highScores")) || []
+//short circuiting
+
 
 var questions = [
     {
@@ -25,8 +27,28 @@ var questions = [
 ]
 
 var currentQuestion = 0
+var timeLeft = 0
+var maxTime = 30
+var score = 0
+var timerHandle
+var resultDisplayHandle
 
+function setTimeLeft(seconds) {
 
+    timeLeft = Math.max(seconds, 0)
+    document.getElementById("secondsLeft").textContent = timeLeft
+}
+
+function startTimer() {
+    setTimeLeft(maxTime)
+    timerHandle = setInterval(function () {
+        setTimeLeft(timeLeft - 1)
+        if (timeLeft <= 0) {
+            results()
+        }
+    }, 1000)
+
+}
 
 var clearContent = function () {
     contentEl.innerHTML = ""
@@ -38,47 +60,8 @@ function takeQuiz() {
     document.getElementById("questionDiv").style = "display:block";
 
     displayQuestion(currentQuestion)
-    console.log(currentQuestion)
+    startTimer()
 
-    // Quiz Questions Go Here: Create Array!
-    //var questions = [
-
-    //new Question("A very useful tool used during development for printing content to the debugger is:", ["JavaScript", "terminal/bash","for loops", "console.log"], "console.log"),
-    //];
-
-
-
-
-    // get 2nd object: questions[1].q
-
-    //We Start the game with a score of 0.
-    var score = 0;
-
-    //Loop over every question object
-
-    //for (let i = 0; i < questions.length; i++) {
-
-
-    //var h1 = document.querySelector("#question");
-    //question.innerHTML = questions [i].q;
-    //console.log(answer);
-
-    //var choices = questions [i].b;
-
-    //console.log(questions);
-
-    //var button = document.createElement("button");
-    //document.getElementById("choice").appendChild(button);
-    //button.innerHTML = choices;
-    //button.style = "display:block;"
-
-    //var answer= choice.onclick;
-    //console.log(answer);
-    //button.addEventListener('click',function() {
-    //evAnswer(i===question.a)
-    //})
-
-    //}
 }
 
 function displayQuestion(index) {
@@ -102,63 +85,61 @@ function displayQuestion(index) {
     //evaluateAnswer()
 }
 
+
 function evaluateAnswer(correct) {
-    console.log(correct)
+    if (correct) {
+        score = score + 10
+        document.getElementById("result").textContent = "Correct!"
+    } else {
+        setTimeLeft(timeLeft - 10)
+        document.getElementById("result").textContent = "Incorrect...!"
+    }
     currentQuestion++
 
+    document.getElementById("answerResults").style = "display:block"
+    clearTimeout(resultDisplayHandle)
+    resultDisplayHandle = setTimeout(function () {
+        document.getElementById("answerResults").style = ""
+    }, 2000);
 
 
     if (currentQuestion < questions.length) {
         displayQuestion(currentQuestion)
     } else {
-        results ()
+        results()
         console.log("You've finished!")
     }
 };
 
-function results () {
-//change display
+function results() {
+    clearInterval(timerHandle)
+    console.log(score)
+    score = score + timeLeft
 
+    //change display
+    document.getElementById("yourScore").style = "display: block";
+    document.getElementById("quizSection").style = "display:none";
+    document.getElementById("questionDiv").style = "display:none";
+    document.getElementById("score").textContent = score
 }
 
-       //(questions[i]);
-        //if (
-           // (answer === questions[i].a) 
-       //) {
-           //Increase score
-            //score++;
-           //console.log('Correct!');
-       // } else {
-         //console.log('Wrong!');
-       //}
-  // };
 
+function saveScore(event) {
+    event.preventDefault()
+    var initials = document.getElementById("inputInitials").value
+    if (!initials) {
+        return;
+    }
+    console.log(initials)
+    var newHighScore = {initials , score}
+    highScores.push(newHighScore)
+    localStorage.setItem("highScores", JSON.stringify(highScores))
+    document.getElementById("yourScore").style = "display: none";
+    document.getElementById("listScores").style = "display: block";
+}
 
-
-   // }
-
-
-
-    //var question1 = "Commonly used data types DO NOT include __________.";
-    //var listQuestions =["strings", "booleans","alerts", "numbers"]; 
-    //var answer = "alerts"; 
-
-    //var h1 = document.querySelector("#question");
-    //h1.innerHTML = question1;
-
-    //for (var i = 0; i < listQuestions.length; i++) {
-
-           //var button = document.createElement("button");
-           //document.getElementById("choice").appendChild(button);
-           //button.innerHTML = listQuestions [i];
-           //button.style = "display:block;"
-
-
-
-
-
-
-
+document.getElementById("start").addEventListener("click", takeQuiz)
+document.getElementById("initialsForm").addEventListener("submit", saveScore)
 
 
 
